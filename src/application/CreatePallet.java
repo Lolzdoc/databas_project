937f45dc9bb377af527f29af0aad4ec8efb386ce
Hans Rietz <dat12hri@student.lth.se>
2016-04-01 14:25:58 +0200

    fixed gui

--- src/application/CreatePallet.java
+++ src/application/CreatePallet.java
@@ -1,12 +1,9 @@
 package application;
 
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
@@ -38,7 +35,7 @@ public class CreatePallet {
     private TextField prod_date;
 
     @FXML
-    private ListView<?> recepie_list;
+    private ListView<?> recipe_list;
 
     @FXML
     private Button submit;
@@ -50,16 +47,15 @@ public class CreatePallet {
 
     @FXML
     void initialize() {
-        assert blocked_enable != null : "fx:id=\"blocked_enable\" was not injected: check your FXML file 'TopTab33.fxml'.";
-        assert customer_id != null : "fx:id=\"customer_id\" was not injected: check your FXML file 'TopTab33.fxml'.";
-        assert deliv_date != null : "fx:id=\"deliv_date\" was not injected: check your FXML file 'TopTab33.fxml'.";
-        assert location != null : "fx:id=\"location\" was not injected: check your FXML file 'TopTab33.fxml'.";
-        assert prod_date != null : "fx:id=\"prod_date\" was not injected: check your FXML file 'TopTab33.fxml'.";
-        assert recepie_list != null : "fx:id=\"recepie_list\" was not injected: check your FXML file 'TopTab33.fxml'.";
-        assert submit != null : "fx:id=\"submit\" was not injected: check your FXML file 'TopTab33.fxml'.";
+        assert blocked_enable != null : "fx:id=\"blocked_enable\" was not injected: check your FXML file 'CreatePallet.fxml'.";
+        assert customer_id != null : "fx:id=\"customer_id\" was not injected: check your FXML file 'CreatePallet.fxml'.";
+        assert deliv_date != null : "fx:id=\"deliv_date\" was not injected: check your FXML file 'CreatePallet.fxml'.";
+        assert pallet_location != null : "fx:id=\"pallet_location\" was not injected: check your FXML file 'CreatePallet.fxml'.";
+        assert prod_date != null : "fx:id=\"prod_date\" was not injected: check your FXML file 'CreatePallet.fxml'.";
+        assert recipe_list != null : "fx:id=\"recipe_list\" was not injected: check your FXML file 'CreatePallet.fxml'.";
+        assert submit != null : "fx:id=\"submit\" was not injected: check your FXML file 'CreatePallet.fxml'.";
 
 
     }
 
 }
-
---
+++ src/application/FindPallet.java
@@ -0,0 +1,82 @@
+package application;
+
+        import java.net.URL;
+        import java.util.ResourceBundle;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.ListView;
+        import javafx.scene.control.RadioButton;
+        import javafx.scene.control.SplitMenuButton;
+        import javafx.scene.control.TextField;
+        import javafx.scene.text.Text;
+
+
+public class FindPallet {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Text Backed_date_out;
+
+    @FXML
+    private Button block_Button;
+
+    @FXML
+    private Text blocked_out;
+
+    @FXML
+    private TextField customer_id;
+
+    @FXML
+    private Text customer_id_out;
+
+    @FXML
+    private Text deleivery_date_out;
+
+    @FXML
+    private ListView<?> filter_result_list;
+
+    @FXML
+    private RadioButton is_Blocked;
+
+    @FXML
+    private Text location_out;
+
+    @FXML
+    private TextField prod_date_end;
+
+    @FXML
+    private TextField prod_date_start;
+
+    @FXML
+    private SplitMenuButton recipe_list;
+
+
+    @FXML
+    void filterPalletButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert Backed_date_out != null : "fx:id=\"Backed_date_out\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert block_Button != null : "fx:id=\"block_Button\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert blocked_out != null : "fx:id=\"blocked_out\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert customer_id != null : "fx:id=\"customer_id\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert customer_id_out != null : "fx:id=\"customer_id_out\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert deleivery_date_out != null : "fx:id=\"deleivery_date_out\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert filter_result_list != null : "fx:id=\"filter_result_list\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert is_Blocked != null : "fx:id=\"is_Blocked\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert location_out != null : "fx:id=\"location_out\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert prod_date_end != null : "fx:id=\"prod_date_end\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert prod_date_start != null : "fx:id=\"prod_date_start\" was not injected: check your FXML file 'FindPallet.fxml'.";
+        assert recipe_list != null : "fx:id=\"recipe_list\" was not injected: check your FXML file 'FindPallet.fxml'.";
+
+
+    }
+
+}
