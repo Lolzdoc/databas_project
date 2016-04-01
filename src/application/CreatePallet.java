6b6b547bcfb73a2856f5ecf7502b8b8b3fbb0abc
NilssonJohan <johan.skolarbeten@gmail.com>
2016-04-01 16:14:17 +0200

    Worked in CreatePallet

--- src/application/CreatePallet.java
+++ src/application/CreatePallet.java
@@ -1,17 +1,17 @@
 package application;
 
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SplitMenuButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 
 public class CreatePallet {
@@ -56,9 +56,42 @@ public class CreatePallet {
     @FXML
     private Button submit;
 
+    private String currentLocation = null;
+
 
     @FXML
     void submitButtonAction(ActionEvent event) {
+        Integer customerID = null;
+        try{
+            customerID = Integer.parseInt(customer_id.getText());
+        } catch (NumberFormatException e){
+            System.out.println("Felaktigt id");
+        }
+
+        String deliveryDate = deliv_date.getText();
+        if(!isValidDate(deliveryDate)){
+//            Skicka Felmeddelande (Ogiltigt datum)
+            System.out.println("Fel deliv datum");
+        }
+
+        String productionDate = prod_date.getText();
+        if(!isValidDate(productionDate)){
+//            Skicka felmeddelande (Ogiltigt datum)
+            System.out.println("Fel prod datum");
+        }
+        Boolean blockedStatus = blocked_enable.isSelected();
+
+        if(currentLocation == null){
+//            Skicka felmeddelande (Ingen location vald)
+            System.out.println("Fel location");
+        }
+
+        System.out.println(customerID.toString());
+        System.out.println(deliveryDate);
+        System.out.println(productionDate);
+        System.out.println(blockedStatus);
+        System.out.println(currentLocation);
+
     }
 
 
@@ -73,6 +106,17 @@ public class CreatePallet {
         recipe_list.getSelectionModel().clearSelection();
     }
 
+    public static boolean isValidDate(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+
     public void initialize() {
    /*     assert blocked_enable != null : "fx:id=\"blocked_enable\" was not injected: check your FXML file 'CreatePallet.fxml'.";
         assert customer_id != null : "fx:id=\"customer_id\" was not injected: check your FXML file 'CreatePallet.fxml'.";
@@ -82,6 +126,12 @@ public class CreatePallet {
         assert recipe_list != null : "fx:id=\"recipe_list\" was not injected: check your FXML file 'CreatePallet.fxml'.";
         assert submit != null : "fx:id=\"submit\" was not injected: check your FXML file 'CreatePallet.fxml'.";
     */
+        for (final MenuItem item : pallet_location.getItems()) {
+            item.setOnAction((event) -> {
+                currentLocation = item.getText();
+                pallet_location.setText(item.getText());
+            });
+        }
 
     }
 
